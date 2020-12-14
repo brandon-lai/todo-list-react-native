312d8a7feb43841a1f9245c54f689e58233d8c3c
Brandon Lai <brandon.lai@vanderbilt.edu>
2020-12-13 21:41:25 -0600

    implements delete todo functionality on touch of todo

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
-import Header from './components/header'
+import Header from './components/header';
+import TodoItem from './components/todoItem';
 
 export default function App() {
   const [todos, setTodos] = useState([
     {text: 'buy coffee', key: '1'},
     {text: 'buy milk', key: '2'},
     {text: 'exercise', key: '3'},
-  ])
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key);
+    });
+  }
 
   return (
     <View style={styles.container}>
@@ -19,7 +26,7 @@ export default function App() {
           <FlatList
             data={todos}
             renderItem={({ item }) => (
-              <Text>{item.text}</Text>
+              <TodoItem item={item} pressHandler={pressHandler} />
             )}
           />
         </View>
